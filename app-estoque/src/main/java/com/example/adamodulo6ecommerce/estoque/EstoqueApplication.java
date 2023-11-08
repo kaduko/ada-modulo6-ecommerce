c7600a219181578d859cfa7897bb91c871fb8953
Milton <miltonrubro@gmail.com>
2023-11-08 16:55:55 -0300

    alteraçao estoque

--- app-estoque/src/main/java/com/example/adamodulo6ecommerce/estoque/EstoqueApplication.java
+++ app-estoque/src/main/java/com/example/adamodulo6ecommerce/estoque/EstoqueApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdaModulo6EcommerceApplication {
+public class EstoqueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdaModulo6EcommerceApplication.class, args);
+		SpringApplication.run(EstoqueApplication.class, args);
 	}
 
 }
